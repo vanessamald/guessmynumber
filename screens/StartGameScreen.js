73133ccdf81920cb471ebfe834b881e912001d54
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2023-07-17 15:25:00 -0500

    styling the startgamescreen component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,11 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
     return (
-        <View>
-            <TextInput/>
+        <View style={styles.inputContainer}>
+            {/* maxLength sets the number or chars allowed in TextInput */}
+            <TextInput style={styles.numberInput} maxLength={2}/>
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
@@ -12,3 +13,31 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginTop: 100,
+        marginHorizontal: 24,
+        padding: 16,
+        backgroundColor: '#72063c',
+        borderRadius: 8,
+        // only works for Android, comparable to boxShadow in CSS
+        elevation: 4,
+        // for iOS
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.25,
+        shadowRadius: 6
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
+})
